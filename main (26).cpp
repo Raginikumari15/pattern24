180ad8651d4ee530f9b485e3ffecce7715664046
ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-20 21:25:49 +0530

    Add files via upload

---
+++ main (26).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int i=1;
+int j,n;
+cin>>n;
+while(i<=n){
+    char ch = 64+ i;
+j=1;
+while(j<=i){
+
+cout << ch;
+ ch++;
+ j++;
+}
+
+cout<< endl;
+i++;
+
+}
+}
